dcb019fff77b61097ecd9238798c32c2c5f638ed
kpunno <kpunno@myseneca.ca>
2024-01-31 20:49:42 -0500

    update UI

--- src/sidepanel.js
+++ src/sidepanel.js
@@ -28,25 +28,42 @@ function loadElements(data) {
 
   // iterate through objects held in store
   for (key in data) {
+    let span = document.createElement("span");
+
     let button = document.createElement("button");
     button.textContent = key;
-
-    button = stylizeButton(button);
-
+    button = styleNameButton(button);
     button.addEventListener("click", () => {
+      //
+      // TODO: REOPEN WINDOW
+      //
       console.log(`${key} was clicked.`);
     });
-
-    div.appendChild(button);
+    let badge = document.createElement("span");
+    badge.textContent = `${data[key].urls.length} tabs `
+    badge = styleTabCountBadge(badge);
+    span.appendChild(button);
+    span.appendChild(badge);
+    div.appendChild(span);
     div.appendChild(document.createElement("br"));
+    
   }
 }
 
-function stylizeButton(button) {
-  button.classList.add("btn", "p-1", "m-1", "text-white");
+function styleNameButton(button) {
+  button.classList.add("btn", "p-0", "m-2");
   button.style = `
     width:100px; 
-    background-color: 
-    #ab00fe;`;
+    background-color: #b1b7f7;
+    color: #150F7A;
+    `;
   return button;
+}
+
+function styleTabCountBadge(badge) {
+  badge.style = `color: #555baa;`;
+  badge.classList.add(
+    "badge","bg-light", "p-1", "m-1", "rounded-pill"
+    );
+  return badge;
 }
